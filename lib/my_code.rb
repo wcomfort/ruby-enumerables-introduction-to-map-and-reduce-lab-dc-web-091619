db01df4d983795c408ff8ebc813c0ad5020b3f06
Will Comfort <comfortwb21@gmail.com>
2019-09-04 21:47:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,73 @@
 # My Code here....
+def map_to_negativize(source_array)
+  i = 0 
+  new_array = []
+   while i < source_array.length do
+   new_array.push(source_array[i] * -1)
+    i += 1 
+  end 
+  return new_array
+end  
+
+def map_to_no_change(source_array)
+  i = 0 
+  new_array = []
+  while i < source_array.length do
+    new_array.push(source_array[i])
+    i += 1 
+  end
+  return new_array
+end
+
+def map_to_double(source_array)
+  i = 0 
+  new_array = []
+  while i < source_array.length do
+    new_array.push(source_array[i] * 2)
+    i += 1 
+  end 
+  return new_array
+end
+
+def map_to_square(source_array)
+  i = 0 
+  new_array = []
+  while i < source_array.length do 
+    new_array.push(source_array[i] ** 2)
+    i += 1 
+  end
+  return new_array
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  i = 0 
+  while i < source_array.length do 
+   starting_point += source_array[i]
+    i += 1 
+  end 
+  return starting_point
+end
+
+def reduce_to_all_true(source_array)
+ i = 0 
+ while i < source_array.length do 
+    source_array[i]
+   if source_array[i] == false 
+   return false
+ end 
+  i += 1
+ end 
+end 
+  
+def reduce_to_any_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    source_array[i] 
+  if source_array[i] == true
+    return true 
+  else
+    return false
+  end
+  i += 1 
+end 
+end
